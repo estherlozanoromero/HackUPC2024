2d95ce663e0dea11d904d78b879400a891e4a1df
unknown <hector.guillen@estudiantat.upc.edu>
2024-05-04 16:54:38 +0200

    hola

--- Puzzle/src/Main.java
+++ Puzzle/src/Main.java
@@ -1,21 +1,14 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("What is the grid size?");
-        int size = scanner.nextInt();
-        
-        Controller controller = new Controller(size);
-        boolean exit = false;
+    public static void main(String[] args) {            
+        Controller controller = new Controller();
 
-        while (!exit) {
-            controller.clearConsole();
+        while (!controller.getSolved()) {
             controller.printBoard();
 
             controller.move();
         }
+
+        controller.Score();
         
     }
 }
