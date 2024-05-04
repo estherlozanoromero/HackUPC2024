a68e55496e8237070158ef496c38af239cb854f7
esther.lozano.romero <esther.lozano.romero@estudiantat.upc.edu>
2024-05-04 13:47:46 +0200

    domain class

---
+++ Puzzle/src/Board.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Board {
+    public int[][] state;
+    public Board (int num) {
+        state = new int[num][num];
+        random();
+    }
+    public void move(int p) {
+        //TODO hacer move si hay hueco al lado
+    }
+    private void random() {
+        //TODO inicializar random pero con conciencia
+    }
+}
